a31af4dd0b31c701ffcc893a88b1a88c8444ccfd
AndersonRocha33 <anderson_paracuru@hotmail.com>
2025-12-26 14:42:03 -0300

    Adiciona desafio classificador de herói

---
+++ index2.js
@@ -0,0 +1,29 @@
+console.log("Arquivo index2.js está rodando");
+
+let vitoria = 50
+let derrota = 10
+let nivel = ""
+
+function ranking (){
+   return vitoria - derrota
+}
+
+let saldo = ranking()
+
+if(vitoria <= 10){
+        nivel = "Ferro";
+} else if (vitoria >= 11 && vitoria <= 20){
+        nivel = "Bronze";
+} else if (vitoria >= 21 && vitoria <= 50){
+        nivel = "Prata";
+} else if (vitoria >= 51 && vitoria <= 80){
+        nivel = "Ouro";
+}else if (vitoria >= 81 && vitoria <= 90){
+        nivel = "Diamante";
+}else if (vitoria >= 91 && vitoria <= 100){
+        nivel = "Lendário";
+}else {
+        nivel = "Imortal";
+    }
+
+    console.log(`O Herói tem de saldo de ${saldo} vitórias e está no nível de ${nivel}`);
